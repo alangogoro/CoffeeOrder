cf15904035779f8ca0fdead4f541f424a3ea83ce
alangogoro <cayne9160@gmail.com>
2021-09-01 16:04:06 +0800

    Add populateOrders func

--- MVVMCoffee/Controllers/OrdersTableViewController.swift
+++ MVVMCoffee/Controllers/OrdersTableViewController.swift
@@ -10,4 +10,27 @@ import UIKit
 
 class OrdersTableViewController: UITableViewController {
     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        populateOrders()
+    }
+    
+    private func populateOrders() {
+        guard let ordersUrl = URL(string: "https://guarded-retreat-82533.herokuapp.com/orders") else {
+            fatalError("API URL was incorrect")
+        }
+        
+        let resource = Resource<Order>(url: ordersUrl)
+        Webservice().load(resource: resource) { result in
+            switch result {
+            case .success(let orders):
+                print(orders)
+            case .failure(let error):
+                debugPrint(error)
+            }
+        }
+    }
+    
+    
 }
